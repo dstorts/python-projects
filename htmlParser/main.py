bc1fd659dad8ebf6822a2bce3e3d5c75288df40b
Dylan Storts <dylanstorts33@gmail.com>
2024-01-18 23:06:53 -0500

    fixed single typing

--- htmlParser/main.py
+++ htmlParser/main.py
@@ -7,77 +7,82 @@ from html.entities import name2codepoint
 # request doc - https://requests.readthedocs.io/en/latest/
 #
 #
-
+debug = True
 expression_list = ['class,infocard-cell-data',
                    'class,ent-name',
-                   'class,/type/',
-                   'class,/type/',
+                   'class,type-icon type-',
                    'class,cell-total',
-                   'class,cell-num',
-                   'class,cell-num',
-                   'class,cell-num',
-                   'class,cell-num',
-                   'class,cell-num',
                    'class,cell-num']
 pokedex = []
-curr_exp_index = 0
-read_data = False
-is_single_type = False
-
 pdata = []
+read_data = False
+type_num = 0
+stat_count = 0
+data_type = ''
 
 class PokemonDBHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
-        global expression_list, curr_exp_index, read_data, is_single_type
-        print("Start tag:", tag)
-        exp = expression_list[curr_exp_index].split(',')
-        attr_type = exp[0]
-        attr_value = exp[1]
-        next_exp = expression_list[curr_exp_index +1].split(',')
-        next_attr_value = next_exp[1]
-
+        global expression_list, read_data, type_num, data_type, debug
+        if debug:
+            print("Start tag:", tag)
         for attr in attrs:
-            print("     attr:", str(attr))
-            # if we are looking for type, but currently seeing total
-            is_single_type = True if attr_value == 'type-icon type-' and next_attr_value in str(attr) else False
-            if attr_type in str(attr) and attr_value in str(attr) and not is_single_type:
-                    print(f"Found: {attr_type}")
-                    print(f"Found: {attr_value}")
+            if debug:
+                print("     attr:", str(attr))
+            for exp in expression_list:
+                exp_tuple = exp.split(',')
+                attr_type = exp_tuple[0]
+                attr_value = exp_tuple[1]
+                if attr_type in str(attr) and attr_value in str(attr):
+                    if debug:
+                        print(f"Found: {attr_value}")
                     read_data = True
+                    data_type = attr_value
+                    if attr_value == 'type-icon type-':
+                        type_num += 1
 
     def handle_data(self, data):
-        global read_data, is_single_type, curr_exp_index, pdata, pokedex
-        print("Data     :", data)
-        if read_data and not is_single_type:
-            print(f"We are saving the data:{data}")
-            pdata.append(data)
+        global read_data, pdata, pokedex, type_num, data_type, stat_count
+        if debug:
+            print("Data     :", data)
+        if read_data:
             read_data = False
-            curr_exp_index += 1
-        if read_data and is_single_type:
-            #here if we are looking at a single type pokemon and have already read the type
-            curr_exp_index += 1 #increment expression index
-            pdata.append('na') #handle non-existent 2nd typing
-            pdata.append(data) #now add the current data, which is the next data for a dual typed pokemon
-            read_data = False
-            is_single_type = False
-        if len(pdata) == 11:
-            #here if we have recorded the 11 sequential datapoints for a single pokemon
-            p = Pokemon()
-            p.dex = pdata[0]
-            p.name = pdata[1]
-            p.type1 = pdata[2]
-            p.type2 = pdata[3]
-            p.total = pdata[4]
-            p.hp = pdata[5]
-            p.atk = pdata[6]
-            p.dfn = pdata[7]
-            p.spatk = pdata[8]
-            p.spdfn = pdata[9]
-            p.spe = pdata[10]
-            pokedex.append(p)
-            print(f"Just saved: {p.name}")
-            curr_exp_index = 0
-            pdata.clear()
+            if debug:
+                print(f"We are saving the data:{data}")
+            if data_type == 'infocard-cell-data':
+                pdata.append(f"dexnum:{data}")
+            elif data_type == 'ent-name':
+                pdata.append(f"name:{data}")
+            elif data_type == 'type-icon type-':
+                pdata.append(f"type:{data}")
+            else:
+                #here if a stat
+                #there are 7 stat numbers
+                #in order, those stats are total, hp, atk, def, spatk, spdef, speed
+                stat_count += 1
+                if   stat_count == 1:
+                    pdata.append(f"total:{data}")
+                elif stat_count == 2:
+                    pdata.append(f"hp:{data}")
+                elif stat_count == 3:
+                    pdata.append(f"atk:{data}")
+                elif stat_count == 4:
+                    pdata.append(f"def:{data}")
+                elif stat_count == 5:
+                    pdata.append(f"spatk:{data}")
+                elif stat_count == 6:
+                    pdata.append(f"spdef:{data}")
+                else:
+                    pdata.append(f"speed:{data}")
+
+        if data_type == 'cell-num':
+            data_type = ''
+            if len(pdata) == 10 and type_num == 1 or len(pdata) == 11 and type_num == 2:
+                #here if we have recorded all datapoints for a single pokemon
+                pokedex.append(pdata)
+                print(f"Just saved: {pdata}")
+                type_num = 0
+                stat_count = 0
+                pdata.clear()
 
 parser = PokemonDBHTMLParser()
 # parser.feed('<html><head><title>Test</title></head>'
@@ -89,7 +94,7 @@ parser = PokemonDBHTMLParser()
 '''
 Looking for: [0]dex num, [1]name, [2]type1, [3]type2, [4]stat total, [5]hp, [6]atk, [7]def, [8]spatk, [9]spdef, [10]spe
 '''
-'''
+
 parser.feed('<table id="pokedex" class="data-table sticky-header block-wide">'
             '<thead>'
             '<tr>'
@@ -108,7 +113,7 @@ parser.feed('<table id="pokedex" class="data-table sticky-header block-wide">'
             '</tr>'
             '</tbody>'
             '</table>')
-'''
+
 parser.feed('<tr>'
             '<td class="cell-num cell-fixed" data-sort-value="4"><span class="infocard-cell-img"><img class="img-fixed icon-pkmn" src="https://img.pokemondb.net/sprites/sword-shield/icon/charmander.png" alt="Charmander" width="56" height="42" loading="lazy"></span><span class="infocard-cell-data">0004</span></td> <td class="cell-name"><a class="ent-name" href="/pokedex/charmander" title="View Pokedex for #0004 Charmander">Charmander</a></td><td class="cell-icon"><a class="type-icon type-fire" href="/type/fire">Fire</a><br> </td>'
             '<td class="cell-num cell-total">309</td>'
@@ -119,8 +124,6 @@ parser.feed('<tr>'
             '<td class="cell-num">50</td>'
             '<td class="cell-num">65</td>'
             '</tr>')
-#ndex_puke = open("pokemon_national_dex_puke.txt","r")
-#parser.feed(ndex_puke.read())
 
-print(f"Name:{pokedex[0].name},dex:{pokedex[0].dex},type1:{pokedex[0].type1},type2:{pokedex[0].type2},total:{pokedex[0].total},hp:{pokedex[0].hp},atk:{pokedex[0].atk},dfn:{pokedex[0].dfn},spatk:{pokedex[0].spatk},spdfn:{pokedex[0].spdfn},speed:{pokedex[0].spe}")
-#print(len(pokedex))
+#ndex_puke = open("pokemon_national_dex_puke.txt", "r")
+#parser.feed(ndex_puke.read())
