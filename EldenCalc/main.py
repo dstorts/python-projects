027a08aac9228defc5ebc0d9d82af120871706cc
Dylan Storts <dylanstorts33@gmail.com>
2024-01-23 22:58:28 -0500

    added working calc correction function

--- EldenCalc/main.py
+++ EldenCalc/main.py
@@ -30,6 +30,7 @@ equip_param_weapon = pandas.read_csv('./data/equip_param_weapon.csv')
 reinforce_param_weapon = pandas.read_csv('./data/reinforce_param_weapon.csv')
 calc_correct_graph_ids = pandas.read_csv('./data/calc_correct_graph_id.csv')
 calc_correct_graph = pandas.read_csv('./data/calc_correct_graph.csv')
+attack_element_correct_param = pandas.read_csv('./data/attack_element_correct_param.csv')
 
 #*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
 weapon_name = 'Cold Dagger'
@@ -48,18 +49,16 @@ def get_weapon_stats(wep_name, print_table):
                 print(f" {equip_weapon.attackBasePhysics}         {equip_weapon.attackBaseMagic}          {equip_weapon.attackBaseFire}             {equip_weapon.attackBaseThunder}               {equip_weapon.attackBaseDark}")
                 print( "Str Scale | Dex Scale | Int Scale   | Fai Scale     | Arc Scaling")
                 print(f" {equip_weapon.correctStrength}         {equip_weapon.correctAgility}          {equip_weapon.correctMagic}            {equip_weapon.correctMagic}              {equip_weapon.correctFaith}")
-            weapon_stats = pandas.Series([equip_weapon.reinforceTypeId, equip_weapon.attackBasePhysics, equip_weapon.attackBaseMagic, equip_weapon.attackBaseFire, equip_weapon.attackBaseThunder, equip_weapon.attackBaseDark, equip_weapon.correctStrength, equip_weapon.correctAgility, equip_weapon.correctMagic, equip_weapon.correctMagic, equip_weapon.correctFaith],
-                                         index=['reinforce_type_id', 'phys_atk', 'magic_atk', 'fire_atk', 'thun_atk', 'holy_atk', 'str_scale', 'dex_scale', 'int_scale', 'fai_scale', 'arc_scale'])
-            return weapon_stats
+            return pandas.Series([equip_weapon.reinforceTypeId, equip_weapon.attackElementCorrectId, equip_weapon.attackBasePhysics, equip_weapon.attackBaseMagic, equip_weapon.attackBaseFire, equip_weapon.attackBaseThunder, equip_weapon.attackBaseDark, equip_weapon.correctStrength, equip_weapon.correctAgility, equip_weapon.correctMagic, equip_weapon.correctMagic, equip_weapon.correctFaith],
+                                         index=['reinforce_type_id', 'attack_element_correct_id', 'phys_atk', 'magic_atk', 'fire_atk', 'thun_atk', 'holy_atk', 'str_scale', 'dex_scale', 'int_scale', 'fai_scale', 'arc_scale'])
 
 def get_upgrading_multipliers(reinforce_type_id, upgrade_level):
     global reinforce_param_weapon
     uid = reinforce_type_id + upgrade_level
     for index, stats in reinforce_param_weapon.iterrows():
         if stats['ID'] == uid:
-            upgrade_multipliers = pandas.Series([stats['Physical Attack'], stats['Magic Attack'], stats['Str Scaling'], stats['Dex Scaling'], stats['Int Scaling'], stats['Fai Scaling'], stats['Arc Scaling']],
-                                                index=['phys_atk', 'magic_atk', 'str_scaler', 'dex_scaler', 'int_scaler', 'fai_scaler', 'arc_scaler'])
-            return upgrade_multipliers
+            return pandas.Series([stats['Physical Attack'], stats['Magic Attack'], stats['Str Scaling'], stats['Dex Scaling'], stats['Int Scaling'], stats['Fai Scaling'], stats['Arc Scaling']],
+                                        index=['phys_atk', 'magic_atk', 'str_scaler', 'dex_scaler', 'int_scaler', 'fai_scaler', 'arc_scaler'])
 
 def get_upgraded_weapon_stats(base_stats, attr_scalers):
     up_phys_atk   = base_stats.phys_atk * attr_scalers.phys_atk
@@ -72,13 +71,98 @@ def get_upgraded_weapon_stats(base_stats, attr_scalers):
 
 def get_calc_correct_graph_ids(wep_name):
     global calc_correct_graph_ids
-    for index, weapon in calc_correct_graph_ids:
+    for index, weapon in calc_correct_graph_ids.iterrows():
         if weapon['Name'] == wep_name:
             #CalcCorrectGraph ID (Physical),CalcCorrectGraph ID (Magic),CalcCorrectGraph ID (Fire),CalcCorrectGraph ID (Lightning),CalcCorrectGraph ID (Holy)
             return pandas.Series([weapon.Physical, weapon.Magic, weapon.Fire, weapon.Lightning, weapon.Holy],
                                  index=['physical', 'magic', 'fire', 'lightning', 'holy'])
 
+def get_calc_correct_params(calc_correct_id):
+    global calc_correct_graph
+    print(calc_correct_id)
+    for index, stats in calc_correct_graph.iterrows():
+        if stats['ID'] == calc_correct_id:
+            return stats
+
+def get_stat_scale_bools(attack_element_id):
+    global attack_element_correct_param
+    for index, element_bools in attack_element_correct_param.iterrows():
+        if element_bools['ID'] == attack_element_id:
+            return element_bools
+
+def helper_get_stat_min(calc_correct_params, player_stat):
+    if player_stat < calc_correct_params['Stat1']:
+        return calc_correct_params['Stat0']
+    elif player_stat < calc_correct_params['Stat2'] and player_stat > calc_correct_params['Stat1']:
+        return calc_correct_params['Stat1']
+    elif player_stat < calc_correct_params['Stat3'] and player_stat > calc_correct_params['Stat2']:
+        return calc_correct_params['Stat2']
+    elif player_stat < calc_correct_params['Stat4'] and player_stat > calc_correct_params['Stat3']:
+        return calc_correct_params['Stat3']
+
+def helper_get_stat_max(calc_correct_params, player_stat):
+    if player_stat < calc_correct_params['Stat1']:
+        return calc_correct_params['Stat1']
+    elif player_stat < calc_correct_params['Stat2'] and player_stat > calc_correct_params['Stat1']:
+        return calc_correct_params['Stat2']
+    elif player_stat < calc_correct_params['Stat3'] and player_stat > calc_correct_params['Stat2']:
+        return calc_correct_params['Stat3']
+    elif player_stat < calc_correct_params['Stat4'] and player_stat > calc_correct_params['Stat3']:
+        return calc_correct_params['Stat4']
+
+def helper_get_grow_min(calc_correct_params, player_stat):
+    if player_stat < calc_correct_params['Stat1']:
+        return calc_correct_params['Grow0']
+    elif player_stat < calc_correct_params['Stat2'] and player_stat > calc_correct_params['Stat1']:
+        return calc_correct_params['Grow1']
+    elif player_stat < calc_correct_params['Stat3'] and player_stat > calc_correct_params['Stat2']:
+        return calc_correct_params['Grow2']
+    elif player_stat < calc_correct_params['Stat4'] and player_stat > calc_correct_params['Stat3']:
+        return calc_correct_params['Grow3']
+
+def helper_get_grow_max(calc_correct_params, player_stat):
+    if player_stat < calc_correct_params['Stat1']:
+        return calc_correct_params['Grow1']
+    elif player_stat < calc_correct_params['Stat2'] and player_stat > calc_correct_params['Stat1']:
+        return calc_correct_params['Grow2']
+    elif player_stat < calc_correct_params['Stat3'] and player_stat > calc_correct_params['Stat2']:
+        return calc_correct_params['Grow3']
+    elif player_stat < calc_correct_params['Stat4'] and player_stat > calc_correct_params['Stat3']:
+        return calc_correct_params['Grow4']
+
+def helper_get_exp_min(calc_correct_params, player_stat):
+    if player_stat < calc_correct_params['Stat1']:
+        return calc_correct_params['Exponent0']
+    elif player_stat < calc_correct_params['Stat2'] and player_stat > calc_correct_params['Stat1']:
+        return calc_correct_params['Exponent1']
+    elif player_stat < calc_correct_params['Stat3'] and player_stat > calc_correct_params['Stat2']:
+        return calc_correct_params['Exponent2']
+    elif player_stat < calc_correct_params['Stat4'] and player_stat > calc_correct_params['Stat3']:
+        return calc_correct_params['Exponent3']
+
+def get_phys_atk_calc_correction_factor(calc_correct_params, player_stat):
+    stat_min = helper_get_stat_min(calc_correct_params, player_stat)
+    stat_max = helper_get_stat_max(calc_correct_params, player_stat)
+    exp_min = helper_get_exp_min(calc_correct_params, player_stat)
+    grow_min = helper_get_grow_min(calc_correct_params, player_stat)
+    grow_max = helper_get_grow_max(calc_correct_params, player_stat)
+
+    ratio = (player_stat - stat_min) / (stat_max - stat_min)
+    if exp_min > 0:
+        growth = ratio**exp_min
+    elif exp_min < 0:
+        growth = 1 - ((1 - ratio)**abs(exp_min))
+    return (grow_min + ((grow_max - grow_min) * growth)) / 100
+
 weapon_stats = get_weapon_stats(weapon_name, False)
+print(f"Attack Element ID: {weapon_stats.attack_element_correct_id}")
 weapon_scalers = get_upgrading_multipliers(weapon_stats.reinforce_type_id, weapon_upgrade_lvl)
 upgraded_weapon_stats = get_upgraded_weapon_stats(weapon_stats, weapon_scalers)
+print(upgraded_weapon_stats)
 calc_correct_ids = get_calc_correct_graph_ids(weapon_name)
+calc_correct_graph_params = get_calc_correct_params(calc_correct_ids.physical)
+#print(calc_correct_graph_params)
+stat_scale_bools = get_stat_scale_bools(weapon_stats.attack_element_correct_id)
+#print(stat_scale_bools)
+str_calc_correction = get_phys_atk_calc_correction_factor(calc_correct_graph_params, player_str)
+print(str_calc_correction)
